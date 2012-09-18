0cda010ff8d055d7d1de97c8c2ddfe696e6c3709
tedsuo <ted@radicaldesigns.org>
2012-09-17 20:58:08 -0700

    test display tweak

--- test/setup_test_display.js
+++ test/setup_test_display.js
@@ -1,11 +1,15 @@
 function setupTestDisplay($){
   $('<a id="test-toggle">')
   .css({
-    position: 'absolute',
     top:0,
     left:0,
     cursor:'pointer',
-    font:'12px monaco'
+    font:'12px monaco',
+    position: 'fixed',
+    background: 'white',
+    'box-shadow': '1px 1px 18px white',
+    padding: '0 7px 3px 4px',
+    'border-radius': '0 0 6px 0'
   })
   .click(function(){
     var $mocha = $('#mocha');
