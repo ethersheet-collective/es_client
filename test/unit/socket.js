0736c4dde9896483c5900bf9648b9e6c9ea40535
ethersheet-collective <ethersheet@ethersheet.org>
2012-12-18 07:49:41 +0000

    websocket tests

--- test/unit/socket.js
+++ test/unit/socket.js
@@ -6,24 +6,40 @@ var config = require('es_client/config');
 var expect = require('chai').expect;
 var should = require('chai').should();
 var sinon = require('sinon');
-var socket = require('es_client/lib/socket').connect();
+var Socket = require('es_client/lib/socket');
+
+var socket = new Socket();
 
 describe('Websockets', function(){
   var websocket, sheet;
 
-  before(function(){
+  beforeEach(function(){
     sheet = new Sheet({socket: socket});
   });
 
-  it('should respond to update cell event', function(){
+  it('should trigger data event when cell is updated', function(){
     var mock = sinon.mock(socket);
-    mock.expects('emit').withArgs('sheet',{
-      action:'update_cell', 
-      params:{id:sheet.id,row_id:sheet.rowAt(0),col_id:sheet.colAt(0),value:1}
+    mock.expects('send').withArgs({
+      id: sheet.id,
+      type: 'sheet',
+      action:'updateCell', 
+      params:[sheet.rowAt(0),sheet.colAt(0),1]
     });
 
     sheet.updateCell(sheet.rowAt(0),sheet.colAt(0),1);
     mock.verify();
+  });
+
+  it('should call correct method on sheet when "sheet" event is emitted', function(){
+    var cell_val = 'over 9000';
+    sheet.getValue(sheet.rowAt(0),sheet.colAt(0)).should.not.equal(cell_val)
+    socket.trigger('sheet', {
+      id: sheet.id,
+      type: 'sheet',
+      action:'updateCell', 
+      params:[sheet.rowAt(0),sheet.colAt(0),cell_val]
+    });
+    sheet.getValue(sheet.rowAt(0),sheet.colAt(0)).should.equal(cell_val)
   });
 });
 
