8290d0ddf07ddca694fa24da5f76fe17627f93d8
cooperq <cooperq@cooperq.com>
2013-02-08 01:52:04 -0800

    making es_command a static thing

--- bin/install.sh
+++ bin/install.sh
@@ -1,8 +1,1 @@
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-cp $DIR/../node_modules/es_command/es_command.js $DIR/../vendor/
+echo 'stub'
