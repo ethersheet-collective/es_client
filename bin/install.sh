389a924148abcef2770813abc48c5b05134147de
cooperq <cooperq@cooperq.com>
2013-02-08 01:41:25 -0800

    trying to fix script to run on travisci

--- bin/install.sh
+++ bin/install.sh
@@ -1,2 +1,8 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 cp $DIR/node_modules/es_command/es_command.js $DIR/vendor/
