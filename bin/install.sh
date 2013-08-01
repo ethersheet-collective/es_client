c4e8becf92884deb8357ac9c68dd3be8d8f1e219
tedsuo <ted@radicaldesigns.org>
2013-08-01 14:19:45 -0700

    add share dependencies to shell scripts

--- bin/install.sh
+++ bin/install.sh
@@ -4,3 +4,5 @@ rm ./vendor/es_expression.js
 ln -s ../node_modules/es_expression/es_expression.js ./vendor/es_expression.js
 rm ./vendor/ref-binder.js
 ln -s ../node_modules/ref-binder/ref_binder.js ./vendor/ref-binder.js
+rm ./vendor/share.js
+ln -s ../node_modules/share/webclient/share.uncompressed.js ./vendor/share.js
--- bin/npm_link.sh
+++ bin/npm_link.sh
@@ -1,3 +1,4 @@
 npm link es_command
 npm link es_expression
 npm link ref-binder
+npm link share 
