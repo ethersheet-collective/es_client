44212ead26faeedefea51fcf5df08dd245922ecf
cooperq <cooperq@cooperq.com>
2014-10-13 12:37:59 -0700

    removing stupid post install script

--- bin/install.sh
+++
@@ -1,6 +0,0 @@
-rm ./vendor/es_command.js
-ln -s ../node_modules/es_command/es_command.js ./vendor/es_command.js
-rm ./vendor/es_expression.js
-ln -s ../node_modules/es_expression/es_expression.js ./vendor/es_expression.js
-rm ./vendor/ref-binder.js
-ln -s ../node_modules/ref-binder/ref_binder.js ./vendor/ref-binder.js
